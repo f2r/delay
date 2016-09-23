b24549660bae47362ceada99303714c70dd5d9e2
fbouchery <fbouchery@ccmbenchmark.com>
2016-09-23 10:12:26 +0200

    Add priority managment

--- src/Delay.php
+++ src/Delay.php
@@ -18,20 +18,27 @@ class Delay
     private static function registerShutdown()
     {
         \register_shutdown_function(function(){
+            // Finish client request if it's possible
             if (function_exists('\fastcgi_finish_request')) {
                 \fastcgi_finish_request();
             }
+
+            // reorder by priority
+            usort(Delay::$callbacks, function ($a, $b) {
+                return $a['priority'] - $b['priority'];
+            });
+            
             foreach (Delay::$callbacks as $callback) {
                 try {
                     \call_user_func_array($callback['callback'], $callback['params']);
                 } catch (\Throwable $e) {
                     // Use for PHP 7 and PHP 5 compatibility. 
-                    if (self::$logger !== null) {
-                        self::$logger->error($e->getMessage());
+                    if (Delay::$logger !== null) {
+                        Delay::$logger->error($e->getMessage());
                     }
                 } catch (\Exception $e) {
-                    if (self::$logger !== null) {
-                        self::$logger->error($e->getMessage());
+                    if (Delay::$logger !== null) {
+                        Delay::$logger->error($e->getMessage());
                     }
                 }
             }
@@ -41,7 +48,7 @@ class Delay
     /**
      * Because, after sending request to user, it's not possible to perform some "echo", attach a logger in order to
      * log callback exceptions
-     * 
+     *
      * @param \Psr\Log\LoggerInterface $logger
      */
     public static function setLogger(LoggerInterface $logger)
@@ -54,8 +61,9 @@ class Delay
      *
      * @param callable $callback    The callable to be called.
      * @param array    $params      The parameters to be passed to the callback, as an indexed array.
+     * @param int      $priority    Callback priority (order by asc). Negative value have higher priority than positive
      */
-    public static function register(callable $callback, array $params = [])
+    public static function register(callable $callback, array $params = [], $priority = 0)
     {
         if (self::$callbacks === null) {
             self::registerShutdown();
@@ -63,7 +71,8 @@ class Delay
         }
         self::$callbacks[] = [
             'callback' => $callback,
-            'params' => $params
+            'params' => $params,
+            'priority' => $priority
         ];
     }
 }
